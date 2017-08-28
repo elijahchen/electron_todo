926f4a524b16df81913d7463a23210557cf79813
elijahchen <code.elijahchen@gmail.com>
2017-08-27 23:49:35 -0400

    -Destructured menu from electron, created menu template

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate); //Ready the menu collection
+  Menu.setApplicationMenu(mainMenu); //Fire up the custom defined menu
 });
+
+const menuTemplate = [
+  {
+    label: 'File'
+  }
+];
